c064a9440f410348cf43e81e848c11ebddb6ec60
asjsandro <asjsandro@gmail.com>
2023-08-27 13:48:42 -0300

    ajustes função busca produto

--- src/app.py
+++ src/app.py
@@ -3,7 +3,8 @@ import pyodbc
 from zeep import Client
 import pprint as pp
 import time
-from lxml import objectify
+from xml.etree import ElementTree
+import re
 
 #Get environment variavel
 config = configparser.ConfigParser()
@@ -14,6 +15,48 @@ user = config['PRODUCAO']['user']
 password = config['PRODUCAO']['password']
 driver = config['PRODUCAO']['driver']
 token = config['PRODUCAO']['token_sic']
+
+def add_root_to_xml(xml_string):
+    return f"<root>{xml_string}</root>"
+
+def normalize_xml_tags(xml_string):
+    # Função que transforma a tag em letras minúsculas
+    def lowercase_tag(match):
+        return match.group(1) + match.group(2).lower() + ">"
+    # Substitui as tags encontradas pela versão em letras minúsculas
+    return re.sub(r"(<\/*)([^>]+)>", lowercase_tag, xml_string)
+
+def consulta_produto(codigo_produto, token):
+    # Definindo o endereço WSDL
+    wsdl = "http://sicbr.com/wsinfoproduto/wsInfoProduto1.asmx?WSDL"
+    client = Client(wsdl)
+
+    # Fazendo a consulta à API SOAP
+    response = client.service.BuscaPorCodigo(Codigo=codigo_produto, Token=token)  # Altere "NomeDoMetodo" pelo método correto.
+
+    # Normalizando a string XML
+    response = normalize_xml_tags(response)
+    response = add_root_to_xml(response)
+
+    # Analisando a string XML
+    response = ElementTree.fromstring(response)
+    # Convertendo o XML para um objeto Python
+    if response.text =='0' :
+        produto = {'root':'0'}
+    else:
+        print(response.text)
+        produto = {
+            "codigo": response.find("codigo").text,
+            "produto": response.find("produto").text,
+            "fabricante": response.find("fabricante").text,
+            "unidade": response.find("unidade").text,
+            "codipi": response.find("codipi").text,
+            "pesobruto": response.find("pesobruto").text or None,
+            "pesoliq": response.find("pesoliq").text or None,
+            "aprovado": response.find("aprovado").text or None,
+            "datainc": response.find("datainc").text
+        }
+    return produto
 
 def DBConnect():
     dbConn = pyodbc.connect(driver=driver, 
@@ -38,21 +81,9 @@ def DBQuery(cur, query, params = []):
         returndata.append(thisrow)
     return returndata
 
-def find_ncm_sic(ean, token):
-    '''
-    creditos: https://webkul.com/blog/python-soap-clients-with-zeep/
-    '''
-    wsdl = 'http://sicbr.com/wsinfoproduto/wsInfoProduto1.asmx?WSDL'
-    client = Client(wsdl=wsdl)
-    result = client.service.BuscaPorCodigo(ean, token).lower()
-    if result == '0':
-        return
-    else:
-        objectxml = objectify.fromstring(result)
-    return objectxml
 if __name__ == '__main__':
     query = '''
-  SELECT
+ SELECT
 	t.codigo,
 	t.produto ,
 	t.fabricante ,
@@ -62,11 +93,12 @@ from
 	TABEST1 t
 where
 	t.codipi is null
-	and t.codigo NOT LIKE  ?
-	and t.fabricante !='SERVICOS'
+	and t.codigo NOT LIKE ?
+	and t.fabricante != 'SERVICOS'
 	and t.codigo not like ?
-ORDER BY t.codigo 
-            '''
+ORDER BY
+	t.codigo
+'''
     cur = DBConnect()
     produtos = DBQuery(cur,query, ['200%','SR.%'])
     print('Total de registros retornado da consulta: ', len(produtos))
@@ -75,6 +107,6 @@ ORDER BY t.codigo
         print('Codigo: ',produto['codigo'])
         print('Descricao: ',produto['produto'])
         ean = produto['codigo']
-        consulta = find_ncm_sic(ean, token)
+        consulta = consulta_produto(ean, token)
         pp.pprint(consulta)
-        # time.sleep(5)
+        time.sleep(2)
